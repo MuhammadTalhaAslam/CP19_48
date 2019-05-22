6bd798dd219a81c8a372806c32e73f327692e7af
TALHA6003 <49781759+TALHA6003@users.noreply.github.com>
2019-05-22 23:59:22 +0500

    Create Boundaryofyellowandblue.py

---
+++ Boundaryofyellowandblue.py
@@ -0,0 +1,7 @@
+#defining the Range of Blue color
+	blue_lower=np.array([99,115,150],np.uint8)
+	blue_upper=np.array([110,255,255],np.uint8)
+
+	#defining the Range of yellow color
+	yellow_lower=np.array([22,60,200],np.uint8)
+	yellow_upper=np.array([60,255,255],np.uint8)
